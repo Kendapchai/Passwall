5e917f644a37fbf76dbc253b68b5b777be486799
Thanhdoanh247 <144571896+Thanhdoanh247@users.noreply.github.com>
2023-10-07 01:10:34 +0700

    Update Passwall2.sh

--- Passwall2.sh
+++ Passwall2.sh
@@ -1,11 +1,14 @@
+# thêm nguồn 
+opkg update
+sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
+echo "src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/generic" >> /etc/opkg/customfeeds.conf
+echo "src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/$(grep "OPENWRT_ARCH" /etc/os-release | awk -F '"' '{print $2}')" >> /etc/opkg/customfeeds.conf
+
+opkg update
+
+wget https://github.com/Thanhdoanh247/Passwall/releases/download/Passwall2/luci-app-passwall2_1.18-1_all.ipk
+opkg install luci-app-passwall2_1.18-1_all.ipk
+
 opkg remove dnsmasq
-opkg install ipset
-opkg install ipt2socks 
-opkg install iptables 
-opkg install iptables-legacy 
-opkg install iptables-mod-conntrack-extra 
-opkg install iptables-mod-iprange 
-opkg install iptables-mod-socket 
-opkg install iptables-mod-tproxy 
-opkg install kmod-ipt-nat 
-opkg install dnsmasq-full
+
+opkg install  ipset ipt2socks iptables iptables-legacy iptables-mod-conntrack-extra iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy kmod-ipt-nat dnsmasq-full
